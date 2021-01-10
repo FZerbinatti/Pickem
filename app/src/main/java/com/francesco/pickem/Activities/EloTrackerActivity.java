0b8e73d8979ea61605c1d42948db4c95ee62e1b2
FZerbinatti <francesco.zerbinatti@gmail.com>
2021-01-10 20:20:28 +0100

    10/01/2020 EloTracker base, graph problems

--- app/src/main/java/com/francesco/pickem/Activities/EloTrackerActivity.java
+++ app/src/main/java/com/francesco/pickem/Activities/EloTrackerActivity.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -16,6 +18,13 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.CatmullRomInterpolator;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYGraphWidget;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.francesco.pickem.Adapters.EloTrackerRecyclerViewAdapter;
 import com.francesco.pickem.Annotation.NonNull;
@@ -31,7 +40,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 
 public class EloTrackerActivity extends AppCompatActivity {
@@ -46,6 +59,10 @@ public class EloTrackerActivity extends AppCompatActivity {
     private String userID;
     private String TAG = "EloTRackerActivity";
     ProgressBar elotrack_progressbar;
+    private XYPlot elotracker_graph;
+
+    ArrayList<EloTracker> elotracker_list;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,14 +72,16 @@ public class EloTrackerActivity extends AppCompatActivity {
         addElo();
         recycler_eloTracker = findViewById(R.id.recycler_eloTracker);
         elotrack_progressbar = findViewById(R.id.elotrack_progressbar);
-
+        elotracker_graph = (XYPlot) findViewById(R.id.elotracker_graph);
         elotrack_progressbar.setVisibility(View.VISIBLE);
 
         setupBottomNavView();
         loadFirebaseDataEloTracker();
+        //loadGraph();
 
 
     }
+
 
     private void loadFirebaseDataEloTracker() {
 
@@ -74,7 +93,8 @@ public class EloTrackerActivity extends AppCompatActivity {
         int year = calendar.get(Calendar.YEAR);
         Log.d(TAG, "onClick: year:"+year);
 
-        ArrayList<EloTracker> elotracker_list = new ArrayList<>();
+        elotracker_list = new ArrayList<>();
+
 
         // load da firebase le regioni
         FirebaseDatabase.getInstance().getReference(getString(R.string.firebase_users))
@@ -93,6 +113,8 @@ public class EloTrackerActivity extends AppCompatActivity {
 
                 }
                 loadListview(elotracker_list);
+
+
 
             }
 
@@ -113,6 +135,7 @@ public class EloTrackerActivity extends AppCompatActivity {
 
         // set up the RecyclerView
 
+
         recycler_eloTracker.setLayoutManager(new LinearLayoutManager(this));
         adapter = new EloTrackerRecyclerViewAdapter(getApplicationContext(), eloTrackerArrayList);
         recycler_eloTracker.setAdapter(adapter);
@@ -131,6 +154,10 @@ public class EloTrackerActivity extends AppCompatActivity {
                     }
                 })
         );
+
+        //loadGraph();
+
+
 
 
     }
@@ -185,4 +212,83 @@ public class EloTrackerActivity extends AppCompatActivity {
         });
 
     }
+
+    private void loadGraph() {
+
+        Log.d(TAG, "loadListview: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        // create a couple arrays of y-values to plot:
+        Number[] domainLabels = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Number[] series2Numbers = {800, 842, 855, 810, 840, 855, 867, 880, 942, 988};
+
+        Number [] domainEloNumbers = new Number[elotracker_list.size()];
+        Number [] eloNumbers = new Number[elotracker_list.size()];
+
+
+        for (int i=0; i< elotracker_list.size(); i++){
+            eloNumbers[i] = (fromEloLpsToInteger(elotracker_list.get(i).getLps(), elotracker_list.get(i).getElo()));
+            Log.d(TAG, "loadGraph: eloNumbers[i]: "+eloNumbers[i]);
+            domainEloNumbers[i] = i;
+            Log.d(TAG, "loadGraph: domainEloNumbers[i]:"+domainEloNumbers[i]);
+        }
+
+
+        // turn the above arrays into XYSeries':
+        // (Y_VALS_ONLY means use the element index as the x value)
+
+        XYSeries series2 = new SimpleXYSeries(
+                //Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+                 Arrays.asList(eloNumbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+
+        // create formatters to use for drawing a series using LineAndPointRenderer
+        // and configure them from xml:
+
+        LineAndPointFormatter series2Format =
+                //new LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels_2);
+               series2Format = new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
+
+        // add an "dash" effect to the series2 line:
+        series2Format.getLinePaint().setPathEffect(new DashPathEffect(new float[] {
+
+                // always use DP when specifying pixel sizes, to keep things consistent across devices:
+                PixelUtils.dpToPix(20),
+                PixelUtils.dpToPix(15)}, 0));
+
+        // just for fun, add some smoothing to the lines:
+        // see: http://androidplot.com/smooth-curves-and-androidplot/
+
+
+        series2Format.setInterpolationParams(
+                new CatmullRomInterpolator.Params(100, CatmullRomInterpolator.Type.Centripetal));
+
+        // add a new series' to the xyplot:
+
+        elotracker_graph.addSeries(series2, series2Format);
+
+        elotracker_graph.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                int i = Math.round(((Number) obj).floatValue());
+                Log.d(TAG, "format: domainLabels[i]"+domainLabels[i]);
+                //return toAppendTo.append(domainLabels[i]);
+                return toAppendTo.append(domainEloNumbers[i]);
+            }
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null;
+            }
+        });
+
+
+    }
+    
+    public Integer fromEloLpsToInteger( Integer lps, String elo){
+        
+        if (elo.equals("Challenger")){
+            return  1000+lps;
+        }
+        
+        return 0;
+    }
+
 }
